16ff0b33e3e3efc1cfc7e5b9a624aff2a1e5fc7d
moinijaz <moin.ijaz9@gmail.com>
2024-07-14 13:04:12 +0500

    Added AND Gate

---
+++ AND/tb/and_tb.v
@@ -0,0 +1,43 @@
+
+module AND_tb;
+
+// Parameters
+localparam WIDTH = 5;
+
+//Ports
+reg  clk; 
+reg  [WIDTH-1:0] a;
+reg  [WIDTH-1:0] b;
+wire [WIDTH:0]   z;
+integer i;
+
+AND #(
+    .WIDTH(WIDTH)
+)
+AND_inst (
+    .a(a),
+    .b(b),
+    .z(z)
+);
+
+always #5 clk = !clk;
+
+initial begin
+    {a, b, clk} = 0;
+    for (i = 0; i < 10; i = i + 1) begin
+        repeat(1) @(negedge clk)
+        a = $random;
+        b = $random;
+    end
+    #100
+    $finish;
+end
+
+initial begin
+    begin
+        $dumpfile("AND.vcd");
+        $dumpvars;
+    end
+end
+
+endmodule
